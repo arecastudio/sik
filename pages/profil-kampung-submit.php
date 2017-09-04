3e41b249b9593351c68f6aa53f488ae8b57fc2af
arecastudio <ondiisrail@gmail.com>
2017-09-05 06:50:59 +0900

    tambah

--- pages/profil-kampung-submit.php
+++ pages/profil-kampung-submit.php
@@ -1,4 +1,7 @@
 <?php
+require_once('inc.php');
+$conn=new mysqli(HOST,USER,PASS,DB);
+if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
 
 	$pum=trim($_POST['keys']);
 	$kampung=trim($_POST['kampung']);
@@ -15,16 +18,31 @@
 	$timur=trim($_POST['timur']);
 	$barat=trim($_POST['barat']);
 
-if (strlen($pum)>0 && strlen($kampung)>0 && strlen($kecamatan)>0 && strlen($kabupaten)>0 && strlen($provinsi)>0 && strlen($tahun)>0 && strlen($hukum)>0 && strlen(peta)>0 && strlen($lat)>0 && strlen($lon)>0 && strlen($selatan)>0 && strlen($utara)>0 && strlen($timur)>0 && strlen($barat)>0  ) {
+if (strlen($pum)>0 && strlen($kampung)>0 && strlen($kecamatan)>0 && strlen($kabupaten)>0 && strlen($provinsi)>0 && strlen($tahun)>0 && strlen($hukum)>0 && strlen($peta)>0 && strlen($lat)>0 && strlen($lon)>0 && strlen($selatan)>0 && strlen($utara)>0 && strlen($timur)>0 && strlen($barat)>0 ) {
 	
-	echo "Siap Simpan Data";
+	$sql="INSERT IGNORE INTO pokok_desa(kode_desa_pum,desa_kelurahan,kecamatan,kabupaten_kota,provinsi,tahun_bentuk,dasar_hukum,peta_resmi_wilayah,lat,lon,utara,selatan,timur,barat)VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?);";
+
+	$stmt=$conn->prepare($sql);
+	$stmt->bind_param('ssssssssssssss',$pum,$kampung,$kecamatan,$kabupaten,$provinsi,$tahun,$hukum,$peta,$lat,$lon,$selatan,$utara,$timur,$barat);
+	if ($stmt->execute()) {
+		?>
+		<div class="alert alert-success alert-dismissible" role="alert">
+		  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		  <strong>Success!</strong> Data berhasil ditambahkan.
+		</div>
+		<?php
+	}
+	$stmt->close();
+
 }else{
 	?>
-<div class="alert alert-danger alert-dismissible" role="alert">
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  <strong>Warning!</strong> Data yang di-input belum lengkap.
-</div>
+	<script type="text/javascript">$('#form-submit-entry-data-kampung')[0].reset();</script>
+	<div class="alert alert-danger alert-dismissible" role="alert">
+	  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	  <strong>Warning!</strong> Data yang di-input belum lengkap.
+	</div>	
 	<?php
 }
 
+$conn->close();
 ?>
